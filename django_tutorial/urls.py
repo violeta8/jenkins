3407ef9715a9e6af759d542db6d619074643636e
José Domingo <josedom24@gmail.com>
2018-10-23 12:07:59 +0200

    v2.1

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-	url(r'^', include('polls.urls')),
-    url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,20 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
+# namespace the polls' urls
 app_name = 'polls'
+
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    # ex: /polls/
+    path('', views.index, name='index'),
+
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
