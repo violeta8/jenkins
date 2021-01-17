f897acd1e5b5907801cf5d8b17f69d8ba695d02a
José Domingo Muñoz <josedom24@gmail.com>
2021-01-17 13:55:48 +0100

    Text explicados

--- polls/tests.py
+++ polls/tests.py
@@ -6,15 +6,44 @@ from django.urls import reverse
 
 from .models import Question
 
-
 def create_question(question_text, days):
     """
     Create a question with the given `question_text` and published the
     given number of `days` offset to now (negative for questions published
     in the past, positive for questions that have yet to be published).
+    
+    Función auxiliar que permite crear una pregunta.
+    Recibe el texto de la pregunta y los días que han pasado o faltan
+    desde o para publicarla. (Un valor negativo significa que ya se publico,
+    un valor positivo significa que se tenfrá que publicar en el futuro.)
     """
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+"""Test para la clase DetailView
+
+La clase DetailView muestra los detalles, pregunta y respuesta de una pregunta. 
+Se accede con la url /polls/<id de la pregunta>.
+
+Requisitos
+==========
+1. Si intentamos ver el detalle de una pregunta con fecha de publicación futura
+ nos debe devolver un 404.
+
+¿Dónde conseguimos esto?
+
+En la función get_queryset de la Class DetailView, la cosulta que se hace es:
+
+    Question.objects.filter(pub_date__lte=timezone.now())
+
+Al no seleccionar ninguna pregunta nos da un 404.
+===============================================================================
+2. Al ver el detalle de una pregunta publicada con un fecha pasada se debe mostrar 
+el texto de la pregunta. Esto se consigue en el template detail.html en la primera línea:
+
+    <h1>{{ question.question_text }}</h1>
+"""
 
 
 class QuestionDetailViewTests(TestCase):
@@ -22,6 +51,10 @@ class QuestionDetailViewTests(TestCase):
         """
         The detail view of a question with a pub_date in the future
         returns a 404 not found.
+
+        En este test se comprueba que si accedemos a detalle de una pregunta 
+        con fecha de publicación en el futuro (todavía no se ha publicado),
+        nos devuelve un 404. 
         """
         future_question = create_question(
             question_text='Future question.', days=5)
@@ -33,6 +66,9 @@ class QuestionDetailViewTests(TestCase):
         """
         The detail view of a question with a pub_date in the past
         displays the question's text.
+
+        Cuando intentamos ver el detalle de una pregunta publicada 
+        en el pasado se debe ver el texto de la pregunta.
         """
         past_question = create_question(
             question_text='Past Question.', days=-5)
@@ -40,11 +76,39 @@ class QuestionDetailViewTests(TestCase):
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
 
+"""Test para la clase IndexView
+La clase IndexView es la encargada de mostrar las preguntas publicadas.
+Cada objeto "Question" tiene una fecha de publicación. 
+
+Solo se publican las preguntas cuya fecha de publicación sea menor que 
+el día actual (publicadas en el pasado). 
+
+En la página principal se ven las 5 últimas preguntas publicadas.
+
+Requisitos
+==========
+1. Si no hay preguntas en la base de datos, en la página principal aparece el mensaje "No polls are available.". 
+Lo puedes ver en el template index.html.
+
+===============================================================================
+2. Si hay preguntas con fecha de publicación en el pasado, debe aparecer en la página principal.
+Eso lo puedes ver en el fichero views.py en la función get_queryset de la Class IndexView, la consulta es:
+
+     Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+===============================================================================
+3. Si tenemos preguntas con fecha de publicación en el futuro, no deben aparecer en la página principal.
+En la página principal aparece el mensaje "No polls are available.". 
+
+"""
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """
         If no questions exist, an appropriate message is displayed.
+
+        Si no hay preguntas en la base de datos debe aparecer el mensaje 
+        "No polls are available."
         """
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
@@ -55,6 +119,9 @@ class QuestionIndexViewTests(TestCase):
         """
         Questions with a pub_date in the past are displayed on the
         index page.
+
+        Las preguntas con fecha de publicación en el pasado debe aparecer en 
+        la página principal.
         """
         create_question(question_text="Past question.", days=-30)
         response = self.client.get(reverse('polls:index'))
@@ -67,6 +134,9 @@ class QuestionIndexViewTests(TestCase):
         """
         Questions with a pub_date in the future aren't displayed on
         the index page.
+
+        Las preguntas con fecha de publicación en el futuro, no deben 
+        aparecer en la página principal.
         """
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
@@ -77,6 +147,9 @@ class QuestionIndexViewTests(TestCase):
         """
         Even if both past and future questions exist, only past questions
         are displayed.
+
+        Si tenemos una pregunta publicada en el pasado y otra en el futuro.
+        Sólo debe aparecer la publicada en el pasado.
         """
         create_question(question_text="Past question.", days=-30)
         create_question(question_text="Future question.", days=30)
@@ -89,6 +162,8 @@ class QuestionIndexViewTests(TestCase):
     def test_two_past_questions(self):
         """
         The questions index page may display multiple questions.
+
+        Si tenemos varias preguntas en el pasado deben aparecer en la página principal.
         """
         create_question(question_text="Past question 1.", days=-30)
         create_question(question_text="Past question 2.", days=-5)
@@ -98,13 +173,38 @@ class QuestionIndexViewTests(TestCase):
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
 
+""" Test sobre el modele de datos
 
+Podemos ver en el modelo de datos (models.py) que la clase Question tiene 
+una función booleana (was_published_recently) que devuelve True si la pregunta ha sido
+publicada en el último día.
+
+Requisitos
+==========
+
+1. En las preguntas publicadas en una fecha futura, la función was_published_recently() 
+debe devolver False
+
+===============================================================================
+
+2. En las preguntas publicadas hace más de 1 día, la función was_published_recently() 
+debe devolver False.
+
+===============================================================================
+
+3. En las preguntas publicadas hace un 1 día, la función was_published_recently() 
+debe devolver True.
+
+"""
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
         is in the future.
+
+        Las preguntas publicadas en una fecha futura, la función 
+        was_published_recently() debe devolver False
         """
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
@@ -114,6 +214,9 @@ class QuestionModelTests(TestCase):
         """
         was_published_recently() returns False for questions whose pub_date
         is older than 1 day.
+        
+        En las preguntas publicadas hace más de 1 día, la función 
+        was_published_recently() debe devolver False.
         """
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_question = Question(pub_date=time)
@@ -123,6 +226,9 @@ class QuestionModelTests(TestCase):
         """
         was_published_recently() returns True for questions whose pub_date
         is within the last day.
+        
+        En las preguntas publicadas hace un 1 día, la función 
+        was_published_recently() debe devolver True.
         """
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
